4be9f99180a19c747994aff583b49f0140fc89b6
EugeniaUvarov <eugenia.uvarov@icloud.com>
2024-11-06 19:06:27 -0800

    Initial Commit

---
+++ Project_03Tests/Project_03Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_03Tests.swift
+//  Project_03Tests
+//
+//  Created by Eugenia Uvarov on 11/6/24.
+//
+
+import Testing
+@testable import Project_03
+
+struct Project_03Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
